9badb970bdfd8713790c3dd6591060bffd4c6c03
Santiago Bonwick <santiago@Santiagos-MacBook-Pro.local>
2026-09-13 22:54:02 +0100

    Wrote foul and timeout engineering functions

---
+++ src/features/fouls.py
@@ -0,0 +1,64 @@
+import pandas as pd
+
+COUNTABLE_FOULS = [
+    "Shooting",
+    "Personal",
+    "Loose Ball",
+    "Offensive",
+    "Personal Take",
+    "Offensive Charge",
+    "Away From Play",
+    "Inbound",
+]
+
+def add_foul_features(df: pd.DataFrame) -> None:
+    df["home_team_fouls_period"] = 0
+    df["away_team_fouls_period"] = 0
+    df["home_in_penalty"] = False
+    df["away_in_penalty"] = False
+
+    current_period = None
+    current_home_fouls = 0
+    current_away_fouls = 0
+    home_in_penalty = False
+    away_in_penalty = False
+
+    df.reset_index(drop=True,inplace=True)
+
+    for index, row in df.iterrows():
+        foul_limit = 5 if row["period"] <= 4 else 4
+
+        if row["period"] != current_period:
+            current_period = row["period"]
+            current_home_fouls = 0
+            current_away_fouls = 0
+            home_in_penalty = False
+            away_in_penalty = False
+
+        elif is_foul_event(row) and is_countable_team_foul(row):
+            if get_foul_committing_side(row) == "home":
+                current_home_fouls += 1
+                if current_home_fouls >= foul_limit:
+                    home_in_penalty = True
+
+            elif get_foul_committing_side(row) == "away":
+                current_away_fouls += 1
+                if current_away_fouls >= foul_limit:
+                    away_in_penalty = True
+
+        df.loc[index, "home_team_fouls_period"] = current_home_fouls
+        df.loc[index, "away_team_fouls_period"] = current_away_fouls
+        df.loc[index, "home_in_penalty"] = home_in_penalty
+        df.loc[index, "away_in_penalty"] = away_in_penalty
+
+
+def is_countable_team_foul(row: pd.Series) -> bool:
+    return pd.notna(row["subType"]) and row["subType"] in COUNTABLE_FOULS
+
+
+def is_foul_event(row: pd.Series) -> bool:
+    return pd.notna(row["actionType"]) and row["actionType"] == "Foul"
+
+
+def get_foul_committing_side(row: pd.Series) -> str:
+    return row["event_side"]
---
+++ src/features/timeouts.py
@@ -0,0 +1,44 @@
+import pandas as pd
+MODERN_TIMEOUTS = ["2017-18","2018-19","2019-20","2020-21","2021-22","2022-23","2023-24","2024-25","2025-26","2026-27"]
+
+
+def add_timeouts(df: pd.DataFrame, era: str) -> None:
+    modern = era in MODERN_TIMEOUTS
+    df["home_timeouts_remaining"] = 7
+    df["away_timeouts_remaining"] = 7
+    home_timeouts = 7
+    away_timeouts = 7
+    period = None
+    df.reset_index(drop=True, inplace=True)
+
+    for index,row in df.iterrows():
+        if row["period"] != period:
+            period = row[period]
+            if period >= 5:
+                home_timeouts = home_timeouts + 2 if modern else home_timeouts + 1
+                away_timeouts = away_timeouts + 2 if modern else away_timeouts + 1
+        if is_team_timeout(row):
+            if row["event_side"] == "home":
+                home_timeouts -= 1
+            elif row["event_side"] == "away":
+                away_timeouts -= 1
+        df.loc[index,"home_timeouts_remaining"] = home_timeouts
+        df.loc[index,"away_timeouts_remaining"] = away_timeouts
+        
+
+def is_team_timeout(row:pd.Series) -> bool:
+    return pd.notna(row["actionType"]) and row["subType"] in ["Regular,Short"]
+
+def parse_timeouts_used(desc: str) -> int:
+    _,data = desc.split("(")
+    temp = ""
+    used = 0
+    for char in data:
+        if char.isnumeric():
+            temp += char
+            continue
+        if temp:
+            used += int(temp)
+            temp = ""
+    return used
+
