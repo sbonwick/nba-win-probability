52dfec942a331664a39f85d68ba54a2863b47de5
Santiago Bonwick <santiago@Santiagos-MacBook-Pro.local>
2026-09-16 23:59:05 +0100

    Started processing data into parquets for model training

--- src/data/data_constants.py
+++ src/data/data_constants.py
@@ -13,3 +13,16 @@ SEASONS = [
 ]
 
 GAME_TYPES = ["Regular Season", "Playoffs"]
+
+TRAINING_SEASONS = [    "2015-16",
+    "2016-17",
+    "2017-18",
+    "2018-19",
+    "2019-20",
+    "2020-21",
+    "2021-22",
+    "2022-23"]
+
+VALIDATION_SEASONS = ["2023-24","2024-25"]
+
+TEST_SEASONS = ["2024-25"]
